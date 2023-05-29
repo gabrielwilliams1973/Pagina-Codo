5c229109f761e98a7c733c7666011a46d8728aa6
gabrielwilliams1973 <gab.dam.wil@hotmail.com>
2023-05-29 19:41:47 -0300

    anexo de validar cantidad numero entero de entrada

---
+++ JS/app.js
@@ -0,0 +1,38 @@
+var valorTicket = 200;
+
+const cantidadDeEntradas = document.getElementById("inputCantidad");
+const categoria = document.getElementById("inputCategoria");
+const calcular = document.getElementById("btnResumen");
+const totalaPagar = document.getElementById("totalaPagar");
+
+function validarCantidad() {
+  const cantidad = cantidadDeEntradas.value.trim();
+
+  if (!cantidad.match(/^\d+$/)) {
+    alert("Ingrese un número entero en la casilla de cantidad de entradas.");
+    cantidadDeEntradas.value = "";
+  }
+}
+
+cantidadDeEntradas.addEventListener("input", validarCantidad);
+
+function calcularTotal() {
+  let total = 0;
+  const cantidad = parseInt(cantidadDeEntradas.value);
+
+  if (isNaN(cantidad) || cantidad % 1 !== 0) {
+    alert("Ingrese un número entero en la casilla de cantidad de entradas.");
+    return;
+  }
+
+  if (categoria.value === "estudiante") {
+    total = cantidadDeEntradas.value * valorTicket * 0.2;
+  } else if (categoria.value === "trainee") {
+    total = cantidadDeEntradas.value * valorTicket * 0.5;
+  } else if (categoria.value === "junior") {
+    total = cantidadDeEntradas.value * valorTicket * 0.85;
+  }
+
+  totalaPagar.innerHTML = "$" + " " + total;
+}
+console.log(cantidadDeEntradas.value);
